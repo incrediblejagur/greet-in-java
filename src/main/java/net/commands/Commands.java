5da28df4726f53143fa0663e451666ec09d3ed9c
incrediblejagur <danielminter13@gmail.com>
2020-06-27 00:38:24 +0200

    moved code to extractor class

--- src/main/java/net/commands/Commands.java
+++ src/main/java/net/commands/Commands.java
@@ -1,53 +1,23 @@
 package net.commands;
-import net.exceptions.GreetException;
-import net.exceptions.NoLanguageExeption;
-import net.exceptions.NoNameException;
 import net.greet.Greet;
 import java.util.Scanner;
 
-public class Commands {
+public class Commands extends Extractor{
 
     public static void home() {
-        Greet greet = new Greet();
         System.out.println("Enter command or type 'help'.");
         Scanner myObj = new Scanner(System.in);
         System.out.print("> ");
         String command = myObj.nextLine();
         if(command.equals("help"))help();
-        if(command.split(" ")[0].equals("greet")){
-            String name = "";
-            String lang = "";
-            try {
-                int greetCommandLength = command.split(" ").length;
-                if(greetCommandLength == 1) throw new NoNameException();
-                name = command.split(" ")[1];
-                if (greetCommandLength == 3) lang = command.split(" ")[2];
-                System.out.println(greet.GreetUser(name, lang));
-                if(greetCommandLength == 2) throw new NoLanguageExeption();
-            }catch (GreetException es){
-                System.out.println(es.getMessage());
-            }
+        if(command.split(" ")[0].equals("greet")) greetPerson(command);
+        if(command.split(" ")[0].equals("greeted")) greeted(command);
+        if(command.split(" ")[0].equals("clear")) clear(command);
+        if(command.equals("counter")){
+            Greet greet = new Greet();
+            System.out.println(greet.totalUniqueUsersGreeted());
         }
         if(command.equals("exit")) return;
-        if(command.split(" ")[0].equals("greeted")){
-            int greetedCommand = command.split(" ").length;
-            if(greetedCommand == 1) {
-                System.out.println(greet.getAllNamesGreeted());
-            }else{
-                String name = command.split(" ")[1];
-                System.out.println(greet.getNameCount(name));
-            }
-        }
-        if(command.split(" ")[0].equals("clear")){
-            int clearCommand = command.split(" ").length;
-            if(clearCommand == 1) {
-                System.out.println(greet.deleteAllNames());
-            }else{
-                String name = command.split(" ")[1];
-                System.out.println(greet.deleteName(name));
-            }
-        }
-        if(command.equals("counter"))System.out.println(greet.totalUniqueUsersGreeted());
         home();
     }
 
