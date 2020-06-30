754a8c820851d469b00a195b307ae3e6bccda121
incrediblejagur <danielminter13@gmail.com>
2020-06-30 16:26:37 +0200

    Update NoLanguageExeption.java

--- src/main/java/net/exceptions/NoLanguageExeption.java
+++ src/main/java/net/exceptions/NoLanguageExeption.java
@@ -4,7 +4,7 @@ public class NoLanguageExeption extends GreetException {
     private final String message;
 
     public NoLanguageExeption() {
-        this.message = "You didn't enter a language, so the default method is 'english'";
+        this.message = "You didn't enter a language, so the default language is 'english'.";
     }
 
     public String getMessage(){
