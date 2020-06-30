49f9f689aa5b18ec1dd92ade18d26bd420ffd75a
incrediblejagur <danielminter13@gmail.com>
2020-06-30 16:27:33 +0200

    color to heading

--- src/main/java/net/main.java
+++ src/main/java/net/main.java
@@ -1,11 +1,12 @@
 package net;
 
 import net.commands.Commands;
+import net.misc.color;
 
 public class main {
 
     public static void main(String[] args) {
-        System.out.println("Greet-In-Java");
+        System.out.println(color.ANSI_GREEN+"Greet-In-Java"+color.ANSI_RESET);
         System.out.println("=============");
         Commands commands = new Commands();
         commands.home();
